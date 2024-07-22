63e36bab628f632a4a85b493322fbee7f4bc72cc
Sarmad <sarmad@gmail.com>
2024-07-21 21:03:46 -0700

    Schema updates

--- data/nhsapp_schema.sql
+++ data/nhsapp_schema.sql
@@ -1,5 +1,34 @@
+CREATE TABLE IF NOT EXISTS organization (
+    organization_id INTEGER PRIMARY KEY,
+    domain_root TEXT,
+    name TEXT,
+    short_name TEXT
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS organization_domain_root ON organization(domain_root);
+
+INSERT INTO organization (domain_root, name, short_name) VALUES('nhshbhs.com', 'Huntington Beach High School', 'HBHS');
+
+
+CREATE TABLE IF NOT EXSITS category (
+    category_id INTEGER PRIMARY KEY,
+    name TEXT,
+    -- FPJS (F=Freshman; P=Sophomore; J=Junior; S=Senior)
+    visibility TEXT,
+    display_order INTEGER
+;
+
+CREATE UNIQUE INDEX IF NOT EXISTS category_name ON category(name);
+
+INSERT INTO category (name, visibility, display_order) VALUES('NHS', 'FPJS', 10);
+INSERT INTO category (name, visibility, display_order) VALUES('Tutoring', 'FPJS', 20);
+INSERT INTO category (name, visibility, display_order) VALUES('Other', 'FPJS', 30);
+INSERT INTO category (name, visibility, display_order) VALUES('Sophomore Project', 'P', 50);
+INSERT INTO category (name, visibility, display_order) VALUES('Senior Cord', 'S', 60);
+
+
 CREATE TABLE IF NOT EXISTS app_user (
-    app_user_id INTEGER PRIMARY KEY,
+    app_user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT NOT NULL,
     full_name TEXT,
     photo_url TEXT,
