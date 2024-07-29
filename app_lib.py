3286f1a08a61cfaea987d22317f348c985f810db
Zane <zaneshaiyen@gmail.com>
2024-07-28 23:12:14 -0700

    View verification logs filters

--- app_lib.py
+++ app_lib.py
@@ -56,7 +56,7 @@ def get_period_by_date(organization_id, date):
     if date is None:
         return None
 
-    query = f"""SELECT academic_year, name, locked_flag, period_id FROM period
+    query = """SELECT academic_year, name, locked_flag, period_id FROM period
                 WHERE
                 organization_id = ? AND ? BETWEEN start_date AND end_date
             """
@@ -72,17 +72,39 @@ def get_period_by_date(organization_id, date):
 # Returns verification_log row factory for user. By default, returns 50 most recent rows.
 #
 def get_verification_logs(organization_id, user_email, name_filter=None, category=None, period=None, row_limit=50):
-    query = f"""SELECT c.name AS category_name, p.name AS period_name, vl.event_name, vl.event_date, vl.event_supervisor, vl.hours_worked, vl.supervisor_signature, vl.location_coords, vl.verification_log_id
+    bindings = []
+
+    query = """SELECT c.name AS category_name, p.name AS period_name, vl.event_name, vl.event_date, vl.event_supervisor, vl.hours_worked, vl.supervisor_signature, vl.location_coords, vl.verification_log_id
                 FROM verification_log vl
-                INNER JOIN app_user u ON u.app_user_id = vl.app_user_id
-                INNER JOIN category c ON c.category_id = vl.category_id
-                INNER JOIN period p ON p.period_id = vl.period_id
-                WHERE u.organization_id = ? AND u.email = ?
+            """
+
+    query += """INNER JOIN app_user u ON u.app_user_id = vl.app_user_id
+            """
+    if name_filter:
+        query += "AND (u.email like ? OR u.full_name like ?)"
+        bindings.append(name_filter)
+        bindings.append(name_filter)
+
+    query += """INNER JOIN category c ON c.category_id = vl.category_id
+            """
+    if category:
+        query += "AND c.name = ?"
+        bindings.append(category)
+
+    query += """INNER JOIN period p ON p.period_id = vl.period_id
+            """
+    if period:
+        query += "AND p.name = ?"
+        bindings.append(period)
+    
+    query += f"""WHERE u.organization_id = ? AND u.email = ?
                 ORDER BY verification_log_id DESC
                 LIMIT {row_limit}
             """
+    bindings.append(organization_id)
+    bindings.append(user_email)
 
-    verification_log_rv = app_db.query_db(query, [organization_id, user_email])
+    verification_log_rv = app_db.query_db(query, bindings)
 
     return verification_log_rv
 
@@ -94,7 +116,7 @@ def add_verification_log(category_name, event_date, hours_worked, event_name, su
 
     ## INSERT verification_log and UPDATE to period_category_user should happen as a single transaction?
 
-    query = f"""INSERT OR IGNORE INTO verification_log
+    query = """INSERT OR IGNORE INTO verification_log
                 (event_name, event_date, event_supervisor, hours_worked, supervisor_signature, location_coords, category_id, app_user_id, period_id, created_by, updated_by)
                 SELECT ?, ?, ?, ?, ?, ?, c.category_id, u.app_user_id, p.period_id, ?, ? FROM app_user u
                 LEFT JOIN period p on p.organization_id = u.organization_id AND ? BETWEEN p.start_date AND p.end_date
@@ -121,7 +143,7 @@ def update_verification_log(verification_log_id, date, category_name, hours_work
 
     ## UPDATE verification_log and UPDATE to period_category_user should happen as a single transaction?
 
-    query = f"""UPDATE verification_log
+    query = """UPDATE verification_log
                 SET hours_worked = ?, updated_by = ?, updated_at=datetime()
                 WHERE verification_log_id = ?
             """
@@ -173,7 +195,7 @@ def update_user_category_hours(date, category_name, organization_id, user_email)
          return None
 
     # By Period/Category for user
-    query = f"""INSERT OR REPLACE INTO period_category_user (period_id, category_id, app_user_id, hours_worked)
+    query = """INSERT OR REPLACE INTO period_category_user (period_id, category_id, app_user_id, hours_worked)
                 SELECT p.period_id, c.category_id, u.app_user_id, sum(vl.hours_worked) FROM verification_log vl
                 INNER JOIN period p ON p.period_id = vl.period_id AND ? BETWEEN p.start_date AND p.end_date
                 INNER JOIN category c ON c.category_id = vl.category_id AND c.name = ?
